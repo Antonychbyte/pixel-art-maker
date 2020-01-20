6398749481eb59ef544c372e47755f5cd740503b
sadko@sadko-N68S3B <anton14024@yandex.ru>
2020-01-20 13:44:47 +0300

    implemented canvas 50*50, added more colors, implemented buttons

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function updateDivColor(event) {
         event.target.style.background = current.style.background;
         event.target.style.borderColor = current.style.background;
     }    
-  
 }
 function createCanvas() {
     let numberOfBlocks = 2501
@@ -35,16 +34,62 @@ function fillPalette() {
         item.className = 'item';
         item.style.background = colors[i];
         itemsWraper.append(item);
-    }              
+    }
+    let colorPicker = document.createElement('div');
+    colorPicker.className = 'colorPicker';
+    colorPicker.textContent = 'select any color ';
+    let input = document.createElement('input');
+    input.type = 'color';
+    colorPicker.append(input)
+    itemsWraper.append(colorPicker);
+    input.addEventListener('change', function() {
+        current.style.background = input.value;
+    });
 }
+function clearCanvas() {
+    let pixels = canvas.querySelectorAll('.pixel');
+    let style = getComputedStyle(canvas);
+    for(let pixel of pixels) {
+        pixel.style.background = style.getPropertyValue('background');
+        pixel.style.borderColor = style.getPropertyValue('borderColor');
+    }
+}
+function save() {
+    localStorage.setItem(JSON.stringify(canvas));
+}
+function load(){
 
-
+}
+function createMenu() {
+    let windowOfMaker = document.querySelector('#windowOfMaker');
+    let menu = document.createElement('div');
+    menu.className = 'menu';
+    windowOfMaker.append(menu);
+    
+    let clearCanvasBtn = document.createElement('button');
+    clearCanvasBtn.name = 'Clear canvas';
+    clearCanvasBtn.innerText = 'Clear';
+    menu.append(clearCanvasBtn);
+    clearCanvasBtn.addEventListener('click', clearCanvas);
+    
+    let saveBtn = document.createElement('button');
+    saveBtn.name = 'Save';
+    saveBtn.innerText = 'Save';
+    saveBtn.addEventListener('click', save);
+    menu.append(saveBtn);
+    
+    let loadBtn = document.createElement('button');
+    loadBtn.name = 'Load';
+    loadBtn.innerText = 'Load';
+    loadBtn.addEventListener('click', load);
+    menu.append(loadBtn);
+}
 createCanvas();
 fillPalette();
+createMenu();
 selectedColor.addEventListener("click", function(event){
     let selectedColorStyle =  getComputedStyle(event.target).getPropertyValue('background-color');
     current.style.background = selectedColorStyle;
-    
 });
 canvas.addEventListener('mousedown', function(event){
     mousedownFlag = 1;
